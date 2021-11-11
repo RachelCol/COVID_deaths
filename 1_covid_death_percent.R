660b4e65ee13fafc0feb2f954416b22afa5e3f9f
Rachel Coleman <coleman.rachel.e@gmail.com>
2021-11-11 09:11:53 -0600

    Made date in title self-updating.

--- 1_covid_death_percent.R
+++ 1_covid_death_percent.R
@@ -5,62 +5,74 @@
 # Percent of deaths due to COVID, All Ages
 combined_plot <- ggplot(combined, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, All Ages, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, All Ages, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 85 Years and Over
 Ages85_and_over_plot <- ggplot(Ages85_and_over, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 85 Years and Over, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 85 Years and Over, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 75-84 Years
 Ages_75_to_84plot <- ggplot(Ages_75_to_84, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 75-84 Years, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 75-84 Years, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 65-74 Years
 Ages_65_to_74plot <- ggplot(Ages_65_to_74, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 65-74 Years, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 65-74 Years, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 55-64 Years
 Ages_55_to_64plot <- ggplot(Ages_55_to_64, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 55-64 Years, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 55-64 Years, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 45-54 Years
 Ages_45_to_54plot <- ggplot(Ages_45_to_54, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 45-54 Years, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 45-54 Years, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 35-44 Years
 Ages_35_to_44plot <- ggplot(Ages_35_to_44, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 35-44 Years, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 35-44 Years, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 25-34 Years
 Ages_25_to_34plot <- ggplot(Ages_25_to_34, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 25-34 Years, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 25-34 Years, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 15-24 Years
 Ages_15_to_24plot <- ggplot(Ages_15_to_24, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 15-24 Years, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 15-24 Years, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 5-14 Years
 Ages_5_to_14plot <- ggplot(Ages_5_to_14, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 5-14 Years, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 5-14 Years, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, 1-4 Years
 Ages_1_to_4plot <- ggplot(Ages_1_to_4, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, 1-4 Years, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, 1-4 Years, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 # Percent of deaths due to COVID, Under 1 year
 Ages_under_1plot <- ggplot(Ages_under_1, aes(x=week_number, y=percent_deaths_covid, group = 1)) +
   geom_line() + xlab("weeks") + ylab("percent") + 
-  ggtitle("COVID deaths as a percent of Total Deaths, Under 1 year, \n weeks ending 2/22/20 to 10/09/21")
+  ggtitle(paste("COVID deaths as a percent of Total Deaths, Under 1 year, \n weeks ending", 
+          COVID[1, 4], "to", COVID[nrow(COVID), 4]))
 
 
 # Graphs
@@ -107,8 +119,9 @@ ages_over_time <- ggplot(data = SEGMENTS, aes(x = weeks)) +
   xlab('Weeks') +
   ylab('Percent') +
   scale_y_continuous(limits = c(0,40)) + 
-  ggtitle("COVID deaths as a percent of all deaths, \n weeks ending 2/22/20 to 10/09/21") 
-
+  ggtitle(paste("COVID deaths as a percent of all deaths, \n weeks ending", 
+                COVID[1, 4], "to", COVID[nrow(COVID), 4]))
+          
 print(ages_over_time)
 
 pdf(file="charts/covid_deaths_as_percent_of_all_deaths.pdf")
